a202a0adb8a5e24741f775b752de05e98b90b81b
Jose-Gustavo-Ramirez <ramirez.gusty@gmail.com>
2022-04-27 23:37:18 -0600

    actualizar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
+import { useState } from "react";
 
 function App() {
+
+  const [contador, setcontador] = useState(0);
   return (
     <div className="App">
       <h1>LISTA DE POST</h1>
+      <h2>{contador}</h2>
     </div>
   );
 }
