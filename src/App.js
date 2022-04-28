49fad3337d123d7052451cb9b70c3b8eb4b4cd7e
Jose-Gustavo-Ramirez <ramirez.gusty@gmail.com>
2022-04-28 00:12:27 -0600

    act

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import { useState } from "react";
 import './App.css'
+
 function App() {
 
   const [contador, setcontador] = useState(0);
+
+  //funcion de flecha
+const incremento = (previo) =>{return previo + 1};
+const disminuir = (previo) =>{return previo - 1};
+
+
   return (
     <div className="App">
       <h1>LISTA DE POST</h1>
       <h2>{contador}</h2>
-      <button className="btn-incremento">+</button>
-      <button className="btn-incremento">-</button>
+      <button onClick={() => {setcontador(incremento)}} className="btn-incremento">+</button>
+      <button onClick={() => {setcontador(disminuir)}}className="btn-incremento">-</button>
     </div>
   );
 }
